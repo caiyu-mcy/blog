76c7d3c6f852d9e787a97b3f142a2c929cdfe91e
zhenbao.xy <zhenbao.xy@alibaba-inc.com>
2017-05-15 21:32:35 +0800

    update source

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-hexo deploy
+# hexo deploy
 git add .
 git commit -m 'update source'
 git push origin master:source
 
+hexo generate
+cp -R public/* .deploy_git
 cd .deploy_git
 git push git@github.com:bluesh/blog.git HEAD:gh-pages
-
-# hexo generate
-# cp -R public/* .deploy/blog
+# cp -R public/* .deploy_git
 # cd public
 # git add .
 # git commit -m 'update gh-pages'
