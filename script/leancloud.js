59ede5c1e63d149f6df947d6519eb40aace9b773
zhenbao.xy <zhenbao.xy@alibaba-inc.com>
2018-08-13 17:47:44 +0800

    Site updated: 2018-08-13 17:47:43

--- script/leancloud.js
+++ script/leancloud.js
@@ -1,31 +1,21 @@
 function showTime(Counter) {
     var query = new AV.Query(Counter);
-    document.querySelectorAll('.leancloud_visitors').forEach(function(item) {
-        var url = item.id.trim();
-        query.equalTo('url', url);
-        query.find({
-            success: function(results) {
-                if (results.length == 0) {
-                    document.getElementById(url).innerHTML = '已读：0';
-                    return;
-                }
-                for (var i = 0; i < results.length; i++) {
-                    var object = results[i];
-                    document.getElementById(url).innerHTML = '已读：' + object.get('time');
-                }
-            },
-            error: function(object, error) {
-                console.log('Error: ' + error.code + ' ' + error.message);
+    query.descending('createdAt');
+    query.find().then(function (results) {
+        results.map(object => {
+            let DOM = document.getElementById(object.get('url'));
+            let time = object.get('time');
+            if(DOM) {
+                DOM.innerHTML = '已读：'+object.get('time')
             }
-        });
-
-    });
+        })
+    })
 }
 
-function addCount(Counter) {
-    var Counter = AV.Object.extend('Counter');
-    url = document.querySelector('.leancloud_visitors').id.trim();
-    title = document.querySelector('.leancloud_visitors').getAttribute('data-title').trim();
+function addCount(Counter, DOMS) {
+    // var Counter = AV.Object.extend('Counter');
+    url = DOMS[0].id.trim();
+    title = DOMS[0].getAttribute('data-title').trim();
     var query = new AV.Query(Counter);
     query.equalTo('url', url);
     query.find({
@@ -64,9 +54,12 @@ function addCount(Counter) {
 }
 (function() {
     var Counter = AV.Object.extend('Counter');
-    if (document.querySelectorAll('.leancloud_visitors').length == 1) {
-        addCount(Counter);
-    } else if (document.querySelectorAll('.leancloud_visitors').length > 1) {
-        showTime(Counter);
+    var DOMS = document.querySelectorAll('.leancloud_visitors');
+
+    if (DOMS.length == 1) {
+        addCount(Counter, DOMS);
+    }
+    else if (DOMS.length > 1) {
+        showTime(Counter, DOMS);
     }
 })();
